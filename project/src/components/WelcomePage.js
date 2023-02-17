e8db311b23824c569a2c385ff7bd03316a116e19
Adam <adamchatila2020@gmail.com>
2023-02-17 15:53:29 +0100

    :/

--- project/src/components/WelcomePage.js
+++ project/src/components/WelcomePage.js
@@ -1,17 +1,22 @@
 import React from "react";
 import welcomebc from '../Assets/welcomebc.jpg'
+import welcomebc2 from '../Assets/welcomebc2.jpg'
 import { Link } from "react-router-dom";
 
 const WelcomePage = () => {
     return (
-    <div className="h-[50%]" >
-        <img src={welcomebc} alt="" />
-      <Link
+    <div>
+        <div class="w-full h-full">
+            <img src={welcomebc} alt="" class="object-cover w-full h-full"/>
+        </div>
+        <div><img src={welcomebc2} alt="" /></div>
+        <div>
+        <Link
         to="/home"
-        className="px-8 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition duration-300"
-      >
+        className="px-8 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600 transition duration-300">
         Go to Home
-      </Link>
+        </Link>
+        </div>
     </div>
   );
 };
